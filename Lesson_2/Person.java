61cc72afd56ab4a9867b07ae2c084b4b69af012d
vassilevpm <vassilevpm@gmail.com>
2022-07-24 13:07:17 +0300

    Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String name = "Павел";
+    String sex = "мужской";
+    double height = 1.94;
+    double weight = 100;
+    int age = 36;
+
+    boolean sleep() {
+        return false;
+    }
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    String run() {
+        return "running";
+    }
+
+    void talk(){
+        System.out.println("talking");
+    }
+
+    void javaLearnining() {
+        System.out.println("learning");
+    }
+}

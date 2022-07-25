1babbb055a4f4773bf2117c209df89329e232ab4
vassilevpm <vassilevpm@gmail.com>
2022-07-25 21:02:59 +0300

    Calculator

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,34 @@
+public class Calculator {
+    public void calculate(int x, int y, char operation) {
+        int result = 1;
+        if(operation == '/' && y == 0) {
+            System.out.println("Error!!! Делить на ноль нельзя!");
+        } else {
+            switch(operation) {
+                case '+' :
+                    result = x + y;
+                    break;
+                case '-' :
+                    result = x - y;
+                    break;
+                case '*' :
+                    result = x * y;
+                    break;
+                case '%' :
+                    result = x % y;
+                    break;
+                case '/' :
+                    result = x / y;
+                    break;
+                case '^' :
+                    int i = 1;
+                    while(i <= y) {
+                        i++;
+                        result *= x;
+                    }
+                    break;
+            }
+            System.out.println(x + " " + operation + " "+ y + " = " + result);
+        }
+    }
+}
---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        String answer;
+        do {
+            System.out.println("Введите первое число:");
+            int x = sc.nextInt();
+            String tmp = sc.nextLine();
+            System.out.println("Введите знак математической операции:");
+            char operation = sc.nextLine().charAt(0);
+            System.out.println("Введите второе число:");
+            int y = sc.nextInt();
+            calculator.calculate(x, y, operation);
+            do {
+                System.out.println("Хотите продолжить вычисления? [yes/no]:");
+                answer = sc.next();
+            } while (!answer.equals("yes") && !answer.equals("no"));
+        } while(answer.equals("yes"));
+    }
+}

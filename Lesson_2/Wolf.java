65f221aff4acd9672771b91017d86816a87f3e8f
vassilevpm <vassilevpm@gmail.com>
2022-07-25 17:12:41 +0300

    Wolf set get

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,9 +1,53 @@
 public class Wolf {
-    String sex;
-    String name;
-    double weight;
-    int age;
-    String color;
+    private String sex;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public void move() {
         System.out.println("Идёт");
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,20 +1,20 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.sex = "male";
-        wolf.name = "Белый клык";
-        wolf.weight = 5.5;
-        wolf.age = 3;
-        wolf.color = "Белый";
+        wolf.setSex("male");
+        wolf.setName("Белый клык");
+        wolf.setWeight(5.5);
+        wolf.setAge(3);
+        wolf.setColor("Белый");
 
-        System.out.println("Волка зовут " + wolf.name);
-        System.out.println("Вес волка составляет " + wolf.weight + " кг");
-        System.out.println("Возраст волка " + wolf.age + " года");
-        System.out.println("Окрас " + wolf.color);
+        System.out.println("Волка зовут " + wolf.getName());
+        System.out.println("Вес волка составляет " + wolf.getWeight() + " кг");
+        System.out.println("Возраст волка " + wolf.getAge() + " года");
+        System.out.println("Окрас " + wolf.getColor());
 
-        System.out.print(wolf.name + " ");
+        System.out.print(wolf.getName() + " ");
         wolf.hawl();
-        System.out.print(wolf.name + " ");
+        System.out.print(wolf.getName() + " ");
         wolf.hunt();
     }
 }

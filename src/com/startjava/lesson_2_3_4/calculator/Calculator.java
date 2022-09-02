c65c6db067c20c01ce4248884cf37c3981cba0e6
vassilevpm <vassilev@yandex.ru>
2022-09-02 18:07:02 +0300

    Calculator first update

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,36 +2,19 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
     
-    public void calculate(int x, int y, char operation) {
-        int result = 1;
-        if(operation == '/' && y == 0) {
-            System.out.println("Error!!! Делить на ноль нельзя!");
-        } else {
-            switch(operation) {
-                case '+' :
-                    result = x + y;
-                    break;
-                case '-' :
-                    result = x - y;
-                    break;
-                case '*' :
-                    result = x * y;
-                    break;
-                case '%' :
-                    result = x % y;
-                    break;
-                case '/' :
-                    result = x / y;
-                    break;
-                case '^' :
-                    int i = 1;
-                    while(i <= y) {
-                        i++;
-                        result *= x;
-                    }
-                    break;
-            }
-            System.out.println(x + " " + operation + " "+ y + " = " + result);
-        }
+    public static int calculate(String mathExpression) {
+        String[] mathArr = mathExpression.split(" ");
+        int x = Integer.parseInt(mathArr[0]);
+        int y = Integer.parseInt(mathArr[2]);
+        char operation = mathArr[1].charAt(0);
+        return switch (operation) {
+            case '+' -> Math.addExact(x, y);
+            case '-' -> Math.subtractExact(x, y);
+            case '*' -> Math.multiplyExact(x, y);
+            case '%' -> Math.floorMod(x, y);
+            case '/' -> Math.floorDiv(x, y);
+            case '^' -> (int) Math.pow(x, y);
+            default -> throw new IllegalStateException("Введен неверный математический знак! " + operation);
+        };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,20 +1,14 @@
 package com.startjava.lesson_2_3_4.calculator;
+
 import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Calculator calculator = new Calculator();
         String answer;
         do {
-            System.out.println("Введите первое число:");
-            int x = sc.nextInt();
-            sc.nextLine();
-            System.out.println("Введите знак математической операции:");
-            char operation = sc.nextLine().charAt(0);
-            System.out.println("Введите второе число:");
-            int y = sc.nextInt();
-            calculator.calculate(x, y, operation);
+            System.out.println("Введите математическое выражение:");
+            System.out.println("Результат вычисления: " + Calculator.calculate(sc.nextLine()));
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
                 answer = sc.next();
